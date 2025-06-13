225315a6bf53f87508cff46d214773c6c58aeeb8
kescob0825 <escobar.karl0825@gmail.com>
2025-06-13 10:43:40 -0700

    WIP: Post call for video processing added

--- video-processing-service/dist/index.js
+++ video-processing-service/dist/index.js
@@ -1,9 +1,35 @@
-import express from 'express';
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+// Post call to upload a video
+app.post("/process-video", (req, res) => {
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+    // Error checking to ensure required fields are not null
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+        return;
+    }
+    // Covert input video to 360p regardless the original resolution
+    ffmpeg(inputFilePath)
+        // Process the video for 360p resolution
+        .outputOptions("-vf", "scale=-1:360")
+        // On end, notify user with 200 OK
+        .on("end", () => {
+        res.status(200).send("Video processing started.");
+    })
+        // On error, notify user with 500 error
+        .on("error", (err) => {
+        console.log(`An error occured: ${err.message}`);
+        res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+        .save(outputFilePath);
 });
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 });
